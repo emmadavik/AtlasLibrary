f7aca7cc97365d0a1a19ed3bc793e4f5713d18c0
Abdisalamilmi1 <abdisalamilmi1@gmail.com>
2026-03-23 14:27:28 +0100

    tagit bort "Våra nyheter på böcker" section, då det känns onödigt för tillfället.

---
+++ items/Migrations/20260317231231_AddDescriptionToCartItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace items.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToCartItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CartItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CartItems");
+        }
+    }
+}
