1bc579fd588e9891f5c8b82ecc06f05084badc70
Emma Davik Andersson <emma.davik@live.se>
2026-03-24 14:45:01 +0100

    css ändringar

--- AtlasLibrary/Program.cs
+++ AtlasLibrary/Program.cs
@@ -1,17 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 
-// HttpClient f�r UsersApi
+// HttpClient för UsersApi
 builder.Services.AddHttpClient("UsersApi", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["ApiSettings:UsersApiBaseUrl"]!);
 });
 
-// HttpClient f�r LoansApi
+// HttpClient för LoansApi
 builder.Services.AddHttpClient("LoansApi", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["ApiSettings:LoansApiBaseUrl"]!);
@@ -21,19 +20,13 @@ builder.Services.AddHttpClient("LoansApi", client =>
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
 
-
+// HttpClient för itemsApi
 builder.Services.AddHttpClient("itemsApi", client =>
 {
     client.BaseAddress = new Uri("http://localhost:5079/");
 });
 
-
-
-
-
-
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -42,20 +35,18 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseSession();
+
 app.UseAuthorization();
 
-
 app.MapStaticAssets();
-
 
 app.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
