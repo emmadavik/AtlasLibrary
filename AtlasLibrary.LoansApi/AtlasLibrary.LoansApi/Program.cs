0d7dd004644600a6ea4e6ab75a8e30c9849ffade
Emma Davik Andersson <emma.davik@live.se>
2026-03-24 09:56:53 +0100

    api.loans

--- AtlasLibrary.LoansApi/AtlasLibrary.LoansApi/Program.cs
+++ AtlasLibrary.LoansApi/AtlasLibrary.LoansApi/Program.cs
@@ -7,8 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+Console.WriteLine("Connection string: " + builder.Configuration.GetConnectionString("DefaultConnection"));
+
 builder.Services.AddDbContext<LoansDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
@@ -22,7 +25,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<LoansDbContext>();
-    db.Database.EnsureCreated();
+    db.Database.Migrate(); //db.Database.EnsureCreated();
 }
 
 app.Run();
