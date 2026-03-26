f245fbc54cb59bc2509837d1861b7286d4a4ab33
Abdisalamilmi1 <abdisalamilmi1@gmail.com>
2026-03-26 15:14:07 +0100

    save before rebase

---
+++ items/Migrations/20260325002503_RemoveItemsTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace items.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveItemsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "CartItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CartItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CartItems");
+        }
+    }
+}
